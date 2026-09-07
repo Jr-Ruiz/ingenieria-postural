ad80636df7d4df165d265f37eedc7c56297df7ea
José Ramón Ruiz <jr.ruizrodriguez@gmail.com>
2026-09-07 10:09:07 +0200

    Desactivar Jekyll con .nojekyll y corregir mayúsculas en base URL

--- astro-config.mjs
+++ astro-config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jr-ruiz.github.io',
+  site: 'https://Jr-Ruiz.github.io',
   base: '/ingenieria-postural',
 });
